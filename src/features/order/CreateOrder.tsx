a49f98011fd7a4fdb6998a6e38f7e19ef41dc23d
bear320 <aaabear320@gmail.com>
2024-08-04 15:58:06 +0800

    feat: 🎸 Set up router

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-// https://uibakery.io/regex-library/phone-number
+// // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (phone: string) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(phone);
 
